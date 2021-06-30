51ee28643f23c8db5d1e34e6edd86019ed9b8336
EFFECT322 <55045221+EFFECT322@users.noreply.github.com>
2021-06-30 17:06:27 +0300

    Update1

--- GraphGUI/MainWindow.java
+++ GraphGUI/MainWindow.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
@@ -7,19 +9,26 @@ import java.util.LinkedList;
 import java.util.Map;
 import javax.swing.*;
 
+enum Mode {
+    Creating,
+    Deleting,
+    Moving,
+    Linking,
+    None
+}
+
 public class MainWindow extends JFrame {
 
     static Graph graph = new Graph();
+    // Режим редактирования.
+    static Mode userMode = Mode.None;
 
-
+    // Окно
     public MainWindow() {
         initUI();
     }
 
     private void initUI() {
-        DrawPanel drawPanel = new DrawPanel();
-        add(drawPanel);
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
@@ -27,27 +36,76 @@ public class MainWindow extends JFrame {
         Dimension appSize = new Dimension(toolkit.getScreenSize().width / 2, toolkit.getScreenSize().height / 2);
         Dimension screenSize = toolkit.getScreenSize();
 
+        // Устанавливаем размеры окна.
         setBounds(screenSize.width / 2 - appSize.width / 2, screenSize.height / 2 - appSize.height / 2, appSize.width, appSize.height);
+
+        // Cоздаём меню.
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu jMenu = new JMenu("Создание");
+        jMenuBar.add(jMenu);
+
+        JMenuItem creating = new JMenuItem("Добавление вершин");
+        JMenuItem deleting = new JMenuItem("Удаление вершин");
+        JMenuItem moving = new JMenuItem("Перемещение вершин");
+        JMenuItem linking = new JMenuItem("Добавление рёбер");
+
+        jMenu.add(creating);
+        jMenu.add(deleting);
+        jMenu.add(moving);
+        jMenu.add(linking);
+
+        creating.addActionListener(e -> userMode = Mode.Creating);
+
+        deleting.addActionListener(e -> userMode = Mode.Deleting);
+
+        moving.addActionListener(e -> userMode = Mode.Moving);
+
+        linking.addActionListener(e -> userMode = Mode.Linking);
+
+        setJMenuBar(jMenuBar);
 
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
-                graph.addNode(e.getX() - 7, e.getY() - 31);
+                switch (userMode) {
+                    case None:
+                        break;
+                    case Creating:
+                        System.out.println(e.getX() + " " + e.getY());
+                        graph.addNode(e.getX() - 7, e.getY() - 52);
+                        break;
+                    case Deleting:
+                        // TODO: удаление
+                        break;
+                    case Moving:
+                        // TODO: перемещение
+                        break;
+                    case Linking:
+                        // TODO: создание рёбер
+                        break;
+                }
             }
         });
+
+        // Создание панели для отображения графа.
+        DrawPanel drawPanel = new DrawPanel();
+        add(drawPanel);
+
+        revalidate();
     }
 
-    public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow();
-    }
-
-    class DrawPanel extends JPanel {
+    // Панель для рисования
+    static class DrawPanel extends JPanel {
 
         private void render(Graphics g) {
             var g2d = (Graphics2D) g;
 
             // Выводим круги.
             g2d.setColor(Color.blue);
+            // Установил сглаживание фигур.
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING , RenderingHints.VALUE_ANTIALIAS_ON);
+
             for (Map.Entry<String, Graph.Node> node : graph.nodes.entrySet()) {
                 Ellipse2D ellipse2D = new Ellipse2D.Double(node.getValue().x - 10, node.getValue().y - 10, 20,  20);
                 g2d.fill(ellipse2D);
@@ -56,6 +114,9 @@ public class MainWindow extends JFrame {
 
             // Выводим имена.
             g2d.setColor(Color.white);
+            // Установил сглаживание текста.
+            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
             for (Map.Entry<String, Graph.Node> node : graph.nodes.entrySet()) {
                 g2d.drawString(node.getValue().name, node.getValue().x - node.getValue().name.length() * 3.5f, node.getValue().y + 5);
             }
@@ -65,41 +126,35 @@ public class MainWindow extends JFrame {
 
         @Override
         public void paintComponent(Graphics g) {
+            super.paintComponent(g);
             render(g);
         }
     }
 
+
+
+    // Граф
     static public class Graph {
 
         static int counter = 0;
-        private HashMap<String, Node> nodes = new HashMap<String, Node>();
+        private final HashMap<String, Node> nodes = new HashMap<>();
 
         public void addNode(int x, int y) {
             nodes.put(Integer.toString(counter), new Node(Integer.toString(counter), x, y));
             counter++;
         }
 
-        public void addEdge(Node firstNode, Node secondNode) {
-            nodes.get(firstNode).addEdge(secondNode);
-        }
-
         public void addEdge(String firstNode, String secondNode) {
             nodes.get(firstNode).addEdge(nodes.get(secondNode));
         }
 
-        public void print() {
-            for (Map.Entry<String, Node> node : nodes.entrySet()) {
-                node.getValue().print();
-            }
-        }
-
-        class Node {
+        static class Node {
 
             int x;
             int y;
             String name;
 
-            LinkedList<Node> childs = new LinkedList<Node>();
+            LinkedList<Node> childs = new LinkedList<>();
 
             Node (String name, int x, int y) {
                 this.name = name;
@@ -112,11 +167,10 @@ public class MainWindow extends JFrame {
                 childs.add(node);
             }
 
-            public void print() {
-                for (Node child : childs) {
-                    System.out.println(this.name + "->" + child.name);
-                }
-            }
         }
+    }
+
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
     }
 }
